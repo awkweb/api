ed475b5f01420898afbdcaf98ae2d640708c88a5
Tom Meagher <thom.meagher@gmail.com>
2019-02-23 12:06:57 -0500

    Tweak prod settings for debug

--- server/settings/prod.py
+++ server/settings/prod.py
@@ -1,7 +1,8 @@
 from .base import *
 
 ALLOWED_HOSTS = [
-    ".wilbur.local",
+    "*",
+    ".wilbur.app",
     # Plaid Webhook Hosts
     "52.21.26.131",
     "52.21.47.157",
@@ -9,3 +10,4 @@ ALLOWED_HOSTS = [
     "52.88.82.239",
 ]
 CORS_ORIGIN_WHITELIST = ("api.wilbur.app", "wilbur.app")
+DEBUG = True
