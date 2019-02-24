82cd37464966da61e1cace32980e8848ad6bbcd6
Tom Meagher <thom.meagher@gmail.com>
2019-02-24 10:03:57 -0500

    Add ec2 ip to allowed hosts

--- server/settings/prod.py
+++ server/settings/prod.py
@@ -1,7 +1,7 @@
+import requests
 from .base import *
 
 ALLOWED_HOSTS = [
-    "*",
     ".wilbur.app",
     # Plaid Webhook Hosts
     "52.21.26.131",
@@ -11,3 +11,12 @@ ALLOWED_HOSTS = [
 ]
 CORS_ORIGIN_WHITELIST = ("api.wilbur.app", "wilbur.app")
 DEBUG = True
+
+EC2_PRIVATE_IP = None
+try:
+    EC2_PRIVATE_IP = requests.get(
+        "http://169.254.169.254/latest/meta-data/local-ipv4", timeout=0.01
+    ).text
+    ALLOWED_HOSTS.append(EC2_PRIVATE_IP)
+except requests.exceptions.RequestException:
+    pass
