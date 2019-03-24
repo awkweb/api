656a2e18ef18b0b112ce7c7ec067b3f4a13a1ad4
Tom Meagher <thom.meagher@gmail.com>
2019-03-24 16:35:28 -0400

    Update allowed hosts for dev

--- server/settings/dev.py
+++ server/settings/dev.py
@@ -1,5 +1,5 @@
 from .base import *
 
-ALLOWED_HOSTS = [".butter.local", ".ngrok.io"]
+ALLOWED_HOSTS = ["127.0.0.1", ".butter.local", ".ngrok.io"]
 CORS_ORIGIN_REGEX_WHITELIST = (r"^(https?://)?(\w+\.)?butter\.local$",)
 DEBUG = True
